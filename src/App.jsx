69206cec396f055553eb42d35e5f2e7674746863
Bilal Ahmed Khan <aamyk47@example.com>
2025-03-04 03:25:59 +0300

    Errors fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
- import LivingRoom from "./Pages/LivingRoom";
- import Bedroom from "./Pages/Bedroom";
- import Dining from "./Pages/Dining";
- import Office from "./Pages/Office";
+import LivingRoom from "./Pages/LivingRoom";
+import Bedroom from "./Pages/Bedroom";
+import Dining from "./Pages/Dining";
+import Office from "./Pages/Office";
 import HomePage from './Pages/Homepage';
 import Cart from "./Pages/Cart";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="">
       <Navbar />
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
-           <Route path="/living-room" element={<LivingRoom />} />
+          <Route path="/living-room" element={<LivingRoom />} />
           <Route path="/bedroom" element={<Bedroom />} />
           <Route path="/dining" element={<Dining />} />
           <Route path="/office" element={<Office />} /> 
